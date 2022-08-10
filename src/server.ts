207d8c6b5d212c0bf3216eb5ae3565c30d9e986e
electrolyte-orb <electrolyte.spdf@outlook.com>
2022-08-10 04:09:01 +0000

    added loaderio config

--- src/server.ts
+++ src/server.ts
@@ -2,6 +2,11 @@ import express from 'express';
 const PORT = process.env.PORT || 8080;
 const app = express();
 
+// loaderio-ce566fc738399261fd36dddc7d5926cb
+app.get('/loaderio-ce566fc738399261fd36dddc7d5926cb', (req, res) => {
+	res.send('loaderio-ce566fc738399261fd36dddc7d5926cb');
+});
+
 app.get('/', (req, res) => {
 	res.contentType('html');
 	res.send('<h1>Hello world</h1>');
